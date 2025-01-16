783e53bd4f5587f1d7c82f4b320e45d8580dda90
sources labs <andr.explore19@gmail.com>
2025-01-16 23:51:47 +0000

    update

--- buildrom.sh
+++ buildrom.sh
@@ -24,10 +24,16 @@ git config --global http.lowSpeedTime 999999   # Tingkatkan waktu low speed
 
 cd ~/android
 repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs
+mkdir -p .repo/local_manifests
+echo '<manifest>
+            <project name="Notkerd69/device_xiaomi_fog-11" path="device/xiaomi/fog" remote="github" revision="lineage-18.1"/>
+            <project name="Asyanx/sea_kernel_xiaomi_sm6225" path="kernel/xiaomi/fog" remote="github" revision="sea-r-oss"/>
+            <project name="https://github.com/Notkerd69/vendor_xiaomi_fog-db" path="vendor/xiaomi/fog" remote="github" revision="lineage-18.1"/>
+            </manifest>' > .repo/local_manifests/roomservice.xml
+#git clone https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b sea-r-oss --depth 1 ~/android/device/xiaomi/fog
+#git clone https://github.com/Notkerd69/device_xiaomi_fog-11 -b lineage-18.1 --depth 1 ~/android/kernel/xiaomi/fog
+#git clone https://github.com/Notkerd69/vendor_xiaomi_fog-db -b lineage-18.1 --depth 1 ~/android/vendor/xiaomi/fog
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle
-git clone https://github.com/Asyanx/sea_kernel_xiaomi_sm6225 -b sea-r-oss --depth 1 ~/android/device/xiaomi/fog
-git clone https://github.com/Notkerd69/device_xiaomi_fog-11 -b lineage-18.1 --depth 1 ~/android/kernel/xiaomi/fog
-git clone https://github.com/Notkerd69/vendor_xiaomi_fog-db -b lineage-18.1 --depth 1 ~/android/vendor/xiaomi/fog
 source build/envsetup.sh
 breakfast fog
 
@@ -35,6 +41,6 @@ export USE_CCACHE=1
 export CCACHE_EXEC=/usr/bin/ccache
 ccache -M 50G
 croot
-#launch lineage_fog-userdebug
-#mka bacon
-brunch fog | tee log.txt
+launch lineage_fog-userdebug
+mka bacon -j8
+#brunch fog | tee log.txt
