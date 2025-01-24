7663ef146316b3de7b4e7f93e57e1b61559d2981
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2025-01-24 19:34:40 +0700

    Update buildrom.sh

--- buildrom.sh
+++ buildrom.sh
@@ -53,8 +53,8 @@ source go-up /proc/config.gz
 #from https://xdaforums.com/t/guide-how-to-build-android-11-with-low-ram.4298483/
 #echo "second attempt"
 #rm -rf packages/resources/devicesettings && git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b lineage-18.1 packages/resources/devicesettings
-cd build/soong && git fetch https://github.com/masemoel/build_soong_legion-r 11;git cherry-pick b45c5ae22f74f1bdbb9bfbdd06ecf7a25033c78b;git cherry-pick b45c5ae22f74f1bdbb9bfbdd06ecf7a25033c78b
-cd /home/circleci
+#cd build/soong && git fetch https://github.com/masemoel/build_soong_legion-r 11;git cherry-pick b45c5ae22f74f1bdbb9bfbdd06ecf7a25033c78b;git cherry-pick b45c5ae22f74f1bdbb9bfbdd06ecf7a25033c78b
+#cd /home/circleci
 # Specify heap size for metalava for R
 # Reduce initial heap size for java from 2048mb to 1024mb
 #git cherry-pick b45c5ae22f74f1bdbb9bfbdd06ecf7a25033c78b
@@ -82,16 +82,12 @@ lunch carbon_fog-userdebug
 #lunch lineage_fog-userdebug
 #croot
 echo "change file for remove the error"
-wget https://raw.githubusercontent.com/GustavoMends/go-up/master/go-up
-source go-up build/make/core/droiddoc.mk
-source go-up build/soong/java/droiddoc.go
-echo "" > build/make/core/droiddoc.mk
-sed -i '824s/<</}/' build/soong/java/droiddoc.go
-sed -i '1667s/>>/}/' build/soong/java/droiddoc.go
-source go-up build/soong/java/droiddoc.go
-
+sed -i '84/4096M/1024M/' build/soong/java/config/config.go
+sed -i '95/-JXmx4096M/-JXmx1024M/' build/soong/java/config/config.go
+sed -i '102/-JXmx4096M/-JXmx1024M/' build/soong/java/config/config.go
+source go-up build/soong/java/config/config.go
 mka bacon -j7 |& tee mka_process20252401_0700.txt
-go-up mka_process20252401_0700.txt
+source go-up mka_process20252401_0700.txt
 #ALLOW_MISSING_DEPENDENCIES=true
 #croot
 #brunch fog | tee log.txt
