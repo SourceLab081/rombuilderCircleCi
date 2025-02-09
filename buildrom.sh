6f7a22640fc8095ffc33690b0be89f2106721566
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2025-02-09 11:18:54 +0700

    Update buildrom.sh

--- buildrom.sh
+++ buildrom.sh
@@ -57,11 +57,11 @@ source rombuilderCircleCi/romsrc.sh
 #repo sync -j$(nproc --all) --force-sync --no-tags --retry=3 
 #repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags --optimized-fetch --prune
-rm -rf device/xiaomi/fog && git clone https://gitlab.com/sourceslab062/device_xiaomi_fog.git --depth 1 -b 14-HorizonDroid device/xiaomi/fog
+rm -rf device/xiaomi/fog && git clone https://gitlab.com/sourceslab062/device_xiaomi_fog2.git --depth 1 -b 14-HorizonDroid device/xiaomi/fog
 git clone https://github.com/SourceLab081/uvite.git --depth 1 -b vauxite-fog kernel/xiaomi/fog
 git clone https://github.com/asterixiverz/vendor_xiaomi_fog.git --depth 1 -b fourteen vendor/xiaomi/fog
 #git clone https://github.com/HorizonDroidLab/system_core.git --depth 1 -b refs/heads/udc system/core
-rm -rf build/soong && git clone https://github.com/DroidX-UI/build_soong.git --depth 1 -b 14 build/soong
+#rm -rf build/soong && git clone https://github.com/DroidX-UI/build_soong.git --depth 1 -b 14 build/soong
 
 #|& tee sync_process20252401_0700.txt
 #wget https://raw.githubusercontent.com/GustavoMends/go-up/master/go-up 
