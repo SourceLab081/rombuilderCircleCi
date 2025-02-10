6bdbab9d9252761ad496905c7015522a97d18bb8
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2025-02-11 00:24:47 +0700

    Update romsrc.sh

--- romsrc.sh
+++ romsrc.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #test file
-#repo init -u https://github.com/LineageOS/android.git -b lineage-18.1 --git-lfs --depth=1
+#repo init -u https://github.com/LineageOS/android.git -b lineage-20.0 --git-lfs --depth=1
 #mkdir -p .repo/local_manifests
 #echo '<manifest>
 #            <project name="Notkerd69/device_xiaomi_fog-11" path="device/xiaomi/fog" remote="github" revision="lineage-18.1"/>
@@ -20,8 +20,8 @@
 #git clone --depth=1 -b cr-9.0 https://github.com/SourceLab081/device_xiaomi_fog-11_2.git device/xiaomi/fog
 #git clone --depth=1 -b lineage-18.1 https://github.com/develux44/vendor_xiaomi_fog-11.git vendor/xiaomi/fog
 #git clone --depth=1 -b 11.0 https://github.com/crdroidandroid/android_hardware_xiaomi.git hardware/xiaomi
-#repo init --depth=1 -u https://github.com/accupara/los18.1 -b lineage-18.1 --git-lfs
-#repo sync -j3 --force-sync --no-tags --retry=3
+repo init --depth=1 -u https://github.com/accupara/los20.0 -b lineage-20.0 --git-lfs
+repo sync -j3 --force-sync --no-tags --retry=3
 repo init --depth=1 -u https://github.com/CarbonROM/android.git -b cr-11.0 --git-lfs
 rm -rf .repo/local_manifests
 git clone https://github.com/SourceLab081/local_manifests --depth 1 -b cr-11.0 .repo/local_manifests
