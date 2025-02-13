c5269db2b2761eba5188d280d48dd46a24632685
...:::01000101:::... <58969724+SourceLab081@users.noreply.github.com>
2025-02-13 13:01:14 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -91,6 +91,9 @@ wget https://github.com/SourceLab081/files/raw/refs/heads/main/30.0.ignore.cil &
 wget https://github.com/SourceLab081/files/raw/refs/heads/main/31.0.ignore.cil && mv 31.0.ignore.cil ../system/sepolicy/private/compat/31.0/31.0.ignore.cil
 wget https://github.com/SourceLab081/files/raw/refs/heads/main/32.0.ignore.cil && mv 32.0.ignore.cil ../system/sepolicy/private/compat/32.0/32.0.ignore.cil
 cd ..
+cp system/sepolicy/private/compat/30.0/30.0.ignore.cil system/sepolicy/prebuilts/api/33.0/private/compat/30.0/30.0.ignore.cil
+cp system/sepolicy/private/compat/31.0/31.0.ignore.cil system/sepolicy/prebuilts/api/33.0/private/compat/31.0/31.0.ignore.cil
+cp system/sepolicy/private/compat/32.0/32.0.ignore.cil system/sepolicy/prebuilts/api/33.0/private/compat/32.0/32.0.ignore.cil
 . build/envsetup.sh
 lunch carbon_fog-userdebug
 #echo "" > build/soong/java/Android.bp
