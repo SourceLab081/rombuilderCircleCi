3cebf9f8e373878b21148b18d2c380c18b2d59a4
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-09-18 16:52:16 +0700

    Update run.sh test VoltageOS-16 2

--- run.sh
+++ run.sh
@@ -110,7 +110,7 @@ rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_
 #rm -rf .repo/local_manifests && git clone https://github.com/SourceLab081/local_manifests --depth 1 -b 16-yaap .repo/local_manifests
 echo "repo sync"
 #view the log 
-#https://circleci.com/api/v1.1/project/circleci/BvV3NeJ7vtWW9UHXraZR4R/FaKTY4NeSUknzeMwS3SSmP/207/output/102/0?file=true&allocation-id=67d2cb13d0604c5a377e6ba4-0-build%2FABCDEFGH
+#https://circleci.com/api/v1.1/project/circleci/BvV3NeJ7vtWW9UHXraZR4R/FaKTY4NeSUknzeMwS3SSmP/220/output/102/0?file=true&allocation-id=67d2cb13d0604c5a377e6ba4-0-build%2FABCDEFGH
 #change for the next log from 193 to 194 , 196 now
 
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags 
