ec545c7db7bae0b177801556ba368f65d8cb21b0
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2025-11-07 05:40:22 +0700

    hybris-11 test8

--- run.sh
+++ run.sh
@@ -256,7 +256,7 @@ breakfast fog userdebug
 #echo "Breakfast + Build the code"
 #brunch fog userdebug
 echo "build the code"
-make -j$(nproc --all) hybris-hal droidmedia libui_compat_layer libsfplugin_ccodec 
+make -j$(nproc --all) hybris-hal droidmedia libui_compat_layer libsfplugin_ccodec libbiometry_fp_api
 #libbiometry_fp_api
 #TARGET_BUILD_GAPPS=true m yaap
 #m atlantis
