bdb1254d69b21bcee4af71153a5099b0349f7387
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2026-01-08 00:50:34 +0700

    test dwnload

--- run.sh
+++ run.sh
@@ -12,19 +12,22 @@ echo 30G | sudo tee /sys/block/zram0/disksize
 sudo mkswap /dev/zram0
 sudo swapon /dev/zram0
 
-sudo apt install -y libc++-dev build-essential git bc kmod cpio flex cpio libncurses5-dev bison libssl-dev libelf-dev curl zip unzip 
-# curl https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh | bash
-wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
-#export kernel_src="--depth=1 -b fog-new https://github.com/SourceLab081/greenforce"
- export kernel_src="--depth=1 -b fog https://github.com/SourceLab081/hybris_kernel"
- export config="vendor/fog-hybris_defconfig"
- export variant="ubt"
- export protonNLos="yes" KSU_NEXT="no" fromFile="yes"
+wget -O AndroidOne-fog-OTA-20260103-1217.zip https://phoenixnap.dl.sourceforge.net/project/dv-work/rom/AndroidOne-fog-OTA-20260103-1217.zip?viasf=1
+wget https://github.com/SourceLab081/files/raw/refs/heads/main/uploadToGithub.sh
+. uploadToGithub.sh AndroidOne-fog-OTA-20260103-1217.zip
+#sudo apt install -y libc++-dev build-essential git bc kmod cpio flex cpio libncurses5-dev bison libssl-dev libelf-dev curl zip unzip 
+## curl https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh | bash
+#wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
+##export kernel_src="--depth=1 -b fog-new https://github.com/SourceLab081/greenforce"
+# export kernel_src="--depth=1 -b fog https://github.com/SourceLab081/hybris_kernel"
+# export config="vendor/fog-hybris_defconfig"
+# export variant="ubt"
+# export protonNLos="yes" KSU_NEXT="no" fromFile="yes"
  
- wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
- export file_src="hybris_kernel_origFog-src.zip"
+# wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
+# export file_src="hybris_kernel_origFog-src.zip"
 
-. build_kernel.sh
+#. build_kernel.sh
 
 
  echo "end"
