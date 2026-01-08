805a5afbcfddd80956d3795991e416f53eec56b6
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2026-01-09 03:50:40 +0700

    build kernel for sfos

--- run.sh
+++ run.sh
@@ -3,31 +3,32 @@ echo "compile kernel start"
 git config --global user.email "tester@localhost"
 git config --global user.name "Tester"
 git config --global color.ui true
-git config --global http.postBuffer 524288000  # Tingkatkan buffer menjadi 500 MB
-git config --global http.lowSpeedLimit 0       # Nonaktifkan batas kecepatan minimum
-git config --global http.lowSpeedTime 999999   # Tingkatkan waktu low speed
+git config --global http.postBuffer 524288000   Tingkatkan buffer menjadi 500 MB
+git config --global http.lowSpeedLimit 0        Nonaktifkan batas kecepatan minimum
+git config --global http.lowSpeedTime 999999    Tingkatkan waktu low speed
 
 sudo modprobe zram
 echo 30G | sudo tee /sys/block/zram0/disksize
 sudo mkswap /dev/zram0
 sudo swapon /dev/zram0
 
-wget -O AndroidOne-fog-OTA-20260103-1217.zip https://phoenixnap.dl.sourceforge.net/project/dv-work/rom/AndroidOne-fog-OTA-20260103-1217.zip?viasf=1
-wget https://github.com/SourceLab081/files/raw/refs/heads/main/uploadToGithub.sh
-. uploadToGithub.sh AndroidOne-fog-OTA-20260103-1217.zip
-#sudo apt install -y libc++-dev build-essential git bc kmod cpio flex cpio libncurses5-dev bison libssl-dev libelf-dev curl zip unzip 
-## curl https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh | bash
-#wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
-##export kernel_src="--depth=1 -b fog-new https://github.com/SourceLab081/greenforce"
-# export kernel_src="--depth=1 -b fog https://github.com/SourceLab081/hybris_kernel"
-# export config="vendor/fog-hybris_defconfig"
-# export variant="ubt"
-# export protonNLos="yes" KSU_NEXT="no" fromFile="yes"
+#wget -O AndroidOne-fog-OTA-20260103-1217.zip https://phoenixnap.dl.sourceforge.net/project/dv-work/rom/AndroidOne-fog-OTA-20260103-1217.zip?viasf=1
+#wget https://github.com/SourceLab081/files/raw/refs/heads/main/uploadToGithub.sh
+#. uploadToGithub.sh AndroidOne-fog-OTA-20260103-1217.zip
+sudo apt install -y libc++-dev build-essential git bc kmod cpio flex cpio libncurses5-dev bison libssl-dev libelf-dev curl zip unzip 
+ #curl https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh | bash
+wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
+ #export kernel_src="--depth=1 -b fog-new https://github.com/SourceLab081/greenforce"
+ #export kernel_src="--depth=1 -b fog https://github.com/SourceLab081/hybris_kernel"
+ export kernel_src="--depth=1 -b fog https://github.com/online0code/halium_kernel"
+ export config="vendor/fog-hybris_defconfig"
+ export variant="ubt"
+ export protonNLos="yes" KSU_NEXT="no" fromFile="no"
  
-# wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
-# export file_src="hybris_kernel_origFog-src.zip"
+ #wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
+ #export file_src="hybris_kernel_origFog-src.zip"
 
-#. build_kernel.sh
+. build_kernel.sh
 
 
  echo "end"
