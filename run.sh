449aa6bbad181969cacc2e12fc7899d2dc9a066b
.:::01000101:::. <58969724+SourceLab081@users.noreply.github.com>
2026-03-21 21:41:40 +0700

    build kernel bpf

--- run.sh
+++ run.sh
@@ -18,11 +18,11 @@ wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
 #export kernel_src="--depth=1 -b fog-new https://github.com/SourceLab081/greenforce"
 #export kernel_src="--depth=1 -b fog https://github.com/SourceLab081/hybris_kernel"
 #export kernel_src="--depth=1 -b fog https://github.com/online0code/halium_kernel"
-export kernel_src="--depth=1 -b vauxite-fog https://github.com/SourceLab081/uvite" 
+export kernel_src="--depth=1 -b main-a16-sm8250-base https://github.com/SourceLab081/sm6225-main" 
 # export config="vendor/fog-hybris_defconfig"
 export config="vendor/fog-perf_defconfig"
-export variant="vauxite"
-export proton="yes" KSU_NEXT="yes" fromFile="no"
+export variant="BPF"
+export proton="yes" KSU_NEXT="no" fromFile="no"
  
  #wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
  #export file_src="hybris_kernel_origFog-src.zip"
