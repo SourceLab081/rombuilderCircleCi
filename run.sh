41710ba090a082fcc5a45211b4f59e3dafe107de
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-05-02 23:36:04 +0700

    #439

--- run.sh
+++ run.sh
@@ -21,13 +21,13 @@ wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
 #export kernel_src="--depth=1 -b main-a16-sm8250-base-ksun-susfs https://github.com/SourceLab081/sm6225-main" 
 #main-a16-sm8250-base
 #main-a16-sm8250-base-ksun-susfs 
-export kernel_src="--depth=1 -b test-fog https://github.com/SourceLab081/greenforce" 
+export kernel_src="--depth=1 -b fog-new2 https://github.com/SourceLab081/greenforce" 
 # bpf_gf-fog https://github.com/SourceLab081/greenforce 
 #export config="vendor/fog-hybris_defconfig-sfos"
-export config="vendor/fog-hybris_defconfig"
-export variant="sfos"
-export eva="no" proton="yes" KSUN_SUSFS="no" fromFile="no"
- # circleci 438
+export config="vendor/fog-perf_defconfig"
+export variant="GreenForce"
+export eva="no" proton="yes" KSUN_SUSFS="yes" fromFile="no"
+ # circleci 439
  #wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
  #export file_src="hybris_kernel_origFog-src.zip"
 
