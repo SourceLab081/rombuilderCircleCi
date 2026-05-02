7a4a1eb42544a09f5345a8e298bc21cffcc02baa
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-05-02 22:20:24 +0700

    #438

--- run.sh
+++ run.sh
@@ -27,7 +27,7 @@ export kernel_src="--depth=1 -b test-fog https://github.com/SourceLab081/greenfo
 export config="vendor/fog-hybris_defconfig"
 export variant="sfos"
 export eva="no" proton="yes" KSUN_SUSFS="no" fromFile="no"
- # circleci 437
+ # circleci 438
  #wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
  #export file_src="hybris_kernel_origFog-src.zip"
 
