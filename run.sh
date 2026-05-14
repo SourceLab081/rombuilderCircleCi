26781ebe1580f29d2e1897675b918d881aeea556
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-05-15 04:34:23 +0700

    #447 halium

--- run.sh
+++ run.sh
@@ -21,13 +21,14 @@ wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
 #export kernel_src="--depth=1 -b main-a16-sm8250-base-ksun-susfs https://github.com/SourceLab081/sm6225-main" 
 #main-a16-sm8250-base
 #main-a16-sm8250-base-ksun-susfs 
-export kernel_src="--depth=1 -b fog-new3 https://github.com/SourceLab081/greenforce" 
+# bpf_gf-fog/arch/arm64/configs/vendor/fog-halium_defconfig
+export kernel_src="--depth=1 -b bpf_gf-fog https://github.com/SourceLab081/greenforce" 
 # bpf_gf-fog https://github.com/SourceLab081/greenforce 
 #export config="vendor/fog-hybris_defconfig-sfos"
-export config="vendor/fog-perf_defconfig"
-export variant="GreenForce"
-export eva="no" proton="yes" KSUN_SUSFS="yes" fromFile="no"
- # circleci 446
+export config="vendor/fog-halium_defconfig"
+export variant="HaliumGf"
+export eva="no" proton="yes" KSUN_SUSFS="no" fromFile="no"
+ # circleci 447
  #wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
  #export file_src="hybris_kernel_origFog-src.zip"
 
