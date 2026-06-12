89d0a3348aafb9f4b9e8487c15f4c2277fb642d3
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-06-12 10:29:09 +0700

    #480

--- run.sh
+++ run.sh
@@ -22,7 +22,7 @@ wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
 #main-a16-sm8250-base
 #main-a16-sm8250-base-ksun-susfs 
 # bpf_gf-fog/arch/arm64/configs/vendor/fog-halium_defconfig
-export config="fog-hybris_defconfig"
+export config="fog-droidian_defconfig"
 export variant="Sfos"
 export eva="no" proton="yes" KSUN_SUSFS="no" PATCH_SUSFS="no" fromFile="no" halium="no"
 
@@ -37,7 +37,7 @@ fi
 #export config="vendor/fog-hybris_defconfig-sfos"
 #export config="vendor/fog-droidian_defconfig"
 #export kernel_src="--depth=1 -b fog https://github.com/SourceLab081/halium_kernel"
- # circleci 479
+ # circleci 480
  #wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
  #export file_src="hybris_kernel_origFog-src.zip"
 
