18a4e692f58bb8eab0205da44dfd7b822db938c9
.::01000101::. <58969724+SourceLab081@users.noreply.github.com>
2026-06-12 07:33:22 +0700

    #474 sfos

--- run.sh
+++ run.sh
@@ -22,14 +22,14 @@ wget https://github.com/SourceLab081/files/raw/refs/heads/main/build_kernel.sh
 #main-a16-sm8250-base
 #main-a16-sm8250-base-ksun-susfs 
 # bpf_gf-fog/arch/arm64/configs/vendor/fog-halium_defconfig
-export config="vendor/fog-perf_defconfig"
-export variant="Gf-KSUNSUSFS"
-export eva="no" proton="yes" KSUN_SUSFS="yes" PATCH_SUSFS="no" fromFile="no" halium="no"
+export config="fog-hybris_defconfig_sfos"
+export variant="Gf-Sfos"
+export eva="no" proton="yes" KSUN_SUSFS="no" PATCH_SUSFS="no" fromFile="no" halium="no"
 
 if [ "$KSUN_SUSFS" = "yes" ]; then
      export kernel_src="--depth 1 --recurse-submodules -b fog_new https://github.com/SourceLab081/greenforce" 
 else
-     export kernel_src="--depth 1 -b fog_new https://github.com/SourceLab081/greenforce" 
+     export kernel_src="--depth 1 -b bpf_gf-fog https://github.com/SourceLab081/greenforce" 
 fi
 
 #export kernel_src="--depth=1 -b fog-new2 https://github.com/SourceLab081/sm6225-main"
@@ -37,7 +37,7 @@ fi
 #export config="vendor/fog-hybris_defconfig-sfos"
 #export config="vendor/fog-droidian_defconfig"
 #export kernel_src="--depth=1 -b fog https://github.com/SourceLab081/halium_kernel"
- # circleci 473
+ # circleci 474
  #wget https://github.com/SourceLab081/uploadz/releases/download/v0.2.2/hybris_kernel_origFog-src.zip
  #export file_src="hybris_kernel_origFog-src.zip"
 
